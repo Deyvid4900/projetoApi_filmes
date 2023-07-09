69d583f0fa5ba92b3e60f9927e04e080accfbb22
Deyvid Oliveira Silva <Deyvid4900@gmail.com>
2023-07-08 21:41:21 -0300

    teste gh-pages

--- MoviePlace/src/main.jsx
+++ MoviePlace/src/main.jsx
@@ -11,7 +11,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
